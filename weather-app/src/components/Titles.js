525b04a18389c3bb3727ff31da573e23bb93df4c
Tope B <topeb@Tomikes-MacBook-Pro.local>
2019-02-24 22:02:21 -0500

    made all components stateless

--- weather-app/src/components/Titles.js
+++ weather-app/src/components/Titles.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-class Titles extends React.Component {
+const Titles = () => (
+	<div>
+		<h1>Weather Finder</h1>
+		<p>Find out temperature, conditions and more...</p>
+	</div>
+);
 
-	render() {
-
-		return(
-			<div>
-				<h1>Weather Finder</h1>
-				<p>Find out temperature, conditions and more...</p>
-			</div>
-		);
-	}
-}
 
 export default Titles;
