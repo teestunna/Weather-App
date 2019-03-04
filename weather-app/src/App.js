563ff540ae88848df5ff76c992285028432e08c3
Tope B <topeb@Tomikes-MacBook-Pro.local>
2019-03-04 03:18:16 -0500

    Added background image CSS

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -28,8 +28,6 @@ class App extends React.Component {
 		const data = await api_call.json();
 
 		if(city && country) {
-			console.log(data);
-
 			this.setState({
 				temperature : data.main.temp,
 				city: data.name,
@@ -76,19 +74,30 @@ class App extends React.Component {
 
 		return(
 			<div>
-				<Titles />
-				<Form getWeather={ this.getWeather }/> 
-				<Weather
-					temperature={ this.state.temperature }
-					city={ this.state.city }
-					country={ this.state.country }
-					humidity={ this.state.humidity }
-					description={ this.state.description }
-					error={ this.state.error }
-				/>
-			</div>
+			<div className="wrapper">
+         		<div className="main">
+            		<div className="container">
+              			<div className="row">
+                			<div className="col-xs-5 title-container">
+								<Titles />
+							</div>
+							<div className="col-xs-7 form-container">
+								<Form getWeather={ this.getWeather } /> 
+								<Weather
+									temperature={ this.state.temperature }
+									city={ this.state.city }
+									country={ this.state.country }
+									humidity={ this.state.humidity }
+									description={ this.state.description }
+									error={ this.state.error }
+								/>
+							</div>
+						</div>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
 		);
-
 	}
 }
 
